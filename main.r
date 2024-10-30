7ab1b56357a586059637cd4c95f74c9e59307277
LoboGuardian <loboguardian.dev@gmail.com>
2024-10-29 20:34:25 -0400

    Assignment

---
+++ main.r
@@ -0,0 +1,17 @@
+# x <- seq(-pi, pi, 0.1)
+# plot(x, cos(x))
+
+n <- 5
+n
+# 15 -> n
+# n <- 15
+# n
+n = n + 5
+n
+
+(10 * 6) / 15
+
+name <- "carmen"; n1 = "wakanda"; n2 <- "name"; m <- 0.5
+# ls()
+
+ls(pat = "m")
